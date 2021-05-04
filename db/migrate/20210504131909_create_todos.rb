5fb2c9544f002a92a771eb392ab437fe50b18b80
jamesncox <jamesncox@yahoo.com>
2021-05-04 08:19:50 -0500

    Generate Todo model

---
+++ db/migrate/20210504131909_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.boolean :done
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
