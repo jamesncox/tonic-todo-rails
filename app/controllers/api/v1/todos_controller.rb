7a9586d8dd9fc0cb8049b2e58dc9017395ede275
jamesncox <jamesncox@yahoo.com>
2021-05-04 08:53:01 -0500

    Update Todo controller with methods

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,5 +1,45 @@
 class Api::V1::TodosController < ApplicationController
 
+    def index
+        @todos = Todo.all
+        render json: @todos, except: [:created_at, :updated_at], status: 200
+    end
+
+    def user_todos
+        @user = User.find_by(id: params[:id])
+        @todos = @user.todos
+        renderf json: @todos, except: [:created_at, :updated_at], status: 200
+    end
+
+    def show
+        @todo = Todo.find_by(id: params[:id])
+        render json: @todo, status: 200
+    end
+
+    def create 
+        @todo = Store.new(todo_params)
+        if @todo.save
+            render json: @todo, status: 200
+        else
+            render json: {errors: @todo.errors.full_messages}, status: 400
+        end
+    end
+
+    def update
+        @todo = Todo.find_by(id: params[:id])
+        if @todo.update(todo_params)
+            render json: @todo, status: 200
+        else
+            render json: {errors: @todo.errors.full_messages}, status: 400
+        end
+    end
+
+    def destroy
+        @todo = Todo.find_by(id: params[:id])
+        @todo.destroy
+        render json: @todo, status: 200
+    end
+
 
     private
         def todo_params
