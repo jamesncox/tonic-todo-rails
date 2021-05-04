ca8b89b798d798f0df13a3a4fb4200a8ed408491
jamesncox <jamesncox@yahoo.com>
2021-05-04 14:29:19 -0500

    Update :users :create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :crate, :destroy]
+      resources :users, only: [:index, :create, :destroy]
       resources :todos
 
       # fetch to custom Todo route for specific user
