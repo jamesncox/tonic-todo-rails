63db4f07f06d62b0dc27244c1502679cf6dae894
jamesncox <jamesncox@yahoo.com>
2021-05-04 08:34:27 -0500

    Update User model with relations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :todos
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password_confirmation, presence: true, on: :create
 end
