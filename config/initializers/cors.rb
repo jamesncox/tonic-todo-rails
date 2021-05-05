d2149bca97c009895841aeb6b2782c14a5b0b117
jamesncox <jamesncox@yahoo.com>
2021-05-05 09:51:17 -0500

    Add the-matrix-todo.netlify.app to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,4 +18,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
+  allow do
+    origins 'https://the-matrix-todo.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
 end
